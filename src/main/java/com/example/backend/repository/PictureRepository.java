cfe34fc9ee94f5b9edf1afd43fd507794464f9bb
alexandre <alexandre.pronce@gmail.com>
2021-12-14 09:56:39 +0100

    repo

---
+++ src/main/java/com/example/backend/repository/PictureRepository.java
@@ -0,0 +1,14 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.Category;
+import com.example.backend.model.Picture;
+import com.example.backend.model.Subcategory;
+import com.example.backend.model.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+public interface PictureRepository extends PagingAndSortingRepository<PictureRepository, ObjectId> {
+    List<Picture>findByIdProduct(ObjectId idProduct) ;
+    void deleteAllByIdProduct(ObjectId idProduct) ;
+}
---
+++ src/main/java/com/example/backend/services/Picture/PictureService.java
@@ -0,0 +1,4 @@
+package com.example.backend.services.Picture;
+
+public interface PictureService {
+}
