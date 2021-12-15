294ecca0b52870fa7557bcbc529f1958aa8374dc
alexandre <alexandre.pronce@gmail.com>
2021-12-15 10:59:24 +0100

    video

---
+++ src/main/java/com/example/backend/repository/VideoRepository.java
@@ -0,0 +1,11 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.Picture;
+import com.example.backend.model.Video;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface VideoRepository extends MongoRepository<Video, ObjectId> {
+    void deleteByIdProduct(ObjectId idProduct) ;
+    Video getVideoByIdProduct(ObjectId idProduct);
+}
